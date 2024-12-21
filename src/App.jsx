d2218ab10d57e6da1f1823084e3a6586d880edc7
YuvamKumarSWE <yuvamk.swe@gmail.com>
2024-12-21 01:07:28 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Header from './Header'
+
+function App() {
+
+  return (
+    <div className="flex flex-col items-center justify-center m-10 ">
+      <Header/>
+    </div>  
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className=''>
+      <h1 className=''>Weather App</h1>
+    </header>
+  );
+}
